c5201035aa55bee375c664df490f8efd4d1650f6
sashakurta <34400974+sashakurta@users.noreply.github.com>
2020-02-12 14:36:00 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,71 +1,114 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.IO;
+using System.Threading.Tasks;
 
-namespace upRead
+namespace CSLab_1
 {
+    static class myCountClass
+    {
+        static Dictionary<char, int> symbolsList = new Dictionary<char, int>();
+        static char Symbol;
+        public static void SymbolsCount(string filetext)
+        {
+            symbolsList.Clear();
+            foreach (var n in filetext)
+            {
+                Symbol = char.ToLower(n);
+                if (symbolsList.ContainsKey(Symbol))
+                    symbolsList[Symbol]++;
+                else
+                    symbolsList.Add(Symbol, 1);
+            }
+        }
+        public static Dictionary<char, double> Probability(Dictionary<char, double> symbols, int size)
+        {
+            foreach (var item in symbolsList)
+            {
+                symbols.Add(item.Key, ((double)item.Value / size));
+            }
+            return symbols;
+        }
+        public static double Entrophy(Dictionary<char, double> symbols)
+        {
+            double entrophy = 0;
+            foreach (var item in symbols)
+            {
+                entrophy += (item.Value * Math.Log(item.Value, 2));
+            }
+            return (entrophy * -1);
+        }
 
+    }
     class Program
     {
-        static readonly char[] base64Table = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O',
-                                              'P','Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d',
-                                              'e','f','g','h','i','j','k','l','m','n','o','p','q','r','s',
-                                              't','u','v','w','x','y','z','0','1','2','3','4','5','6','7',
-                                              '8','9','+','/','=' };
-        static void Base64Convert(string path)
+        static string Read(string path) //Метод зчитує текст з файлу
+
         {
-            string text;
+            string filetext;
             using (StreamReader sr = new StreamReader(path))
             {
-                text = sr.ReadToEnd();
+                filetext = sr.ReadToEnd();
+               
             }
-            byte[] textBytes = Encoding.UTF8.GetBytes(text);
-
-            //string binaryTextBytes1 = textBytes.Select(x => Convert.ToString(x, 2).PadLeft(8, '0'));
-            string binaryTextBytes = string.Join("", textBytes.Select(x => Convert.ToString(x, 2).PadLeft(8, '0')));
-            int countOfBytes = binaryTextBytes.Count();
-            string append = countOfBytes % 3 == 2 ? "==" : countOfBytes % 3 == 1 ? "=" : "";
-
-            int remOfDivision = countOfBytes % 6;
-            if (remOfDivision != 0)
-                for (int i = 0; i < 6 - remOfDivision; i++)
-                {
-                    binaryTextBytes = binaryTextBytes.Insert(countOfBytes, "0");
-                    countOfBytes++;
-                }
-
-            List<string> newList = Enumerable.Range(0, countOfBytes / 6).Select(x => binaryTextBytes.Substring(x * 6, 6)).ToList();
-            string myEnText = string.Join("", newList.Select(x => base64Table[Convert.ToByte(x, 2)])) + append;
-            Console.WriteLine("Текст закодований методом:\n" + myEnText + "\n");
+            return filetext;
         }
-        static void Base64(string path)
+
+
+        static double AmountOfInformation(int amount, double entropy) //Метод підраховує значення кількості інформації в тексті
         {
-            string text;
-            using (StreamReader sr = new StreamReader(path))
-            {
-                text = sr.ReadToEnd();
-            }
-            byte[] textBytes = Encoding.UTF8.GetBytes(text);
-            string enText = Convert.ToBase64String(textBytes);
-            using (StreamWriter sw = new StreamWriter(path.Replace(".tar.bz2", "_base64.tar.bz2"), false))
-            {
-                sw.WriteLine(enText);
-            }
-            Console.WriteLine("Текст закодований задопомогою встроєної бібліотеки:\n" + enText + "\n\n\n");
+            return amount * entropy; ;
         }
+
+        static void DictionaryOut(Dictionary<char, double> symbols)
+        {
+            foreach (var item in symbols)
+            {
+                Console.WriteLine(item.Key + "\t" + item.Value);
+            }
+        }
+        static void AmountOut(double amount, string path)
+        {
+            FileInfo file = new FileInfo(path);
+            double size = file.Length;
+            Console.WriteLine("Кількість інформації в тексті - {0} біт\nЗагальний розмір файлу - {1} біт\nРозмір файлу в - {2} рази більший за кількість інформації", amount, size * 8, size * 8 / amount);
+        }
+      
         static void Main(string[] args)
         {
+            Dictionary<char, double> first = new Dictionary<char, double>();
+            Dictionary<char, double> second = new Dictionary<char, double>();
+            Dictionary<char, double> third = new Dictionary<char, double>();
+            string path1file = "/Users/oleksandrkurta/Университет/3.2/Комп'ютерні системи/Лабораторки/Лабораторна№1/File1/file1.txt";
+            string path2file = "/Users/oleksandrkurta/Университет/3.2/Комп'ютерні системи/Лабораторки/Лабораторна№1/File2/file2.txt";
+            string path3file = "/Users/oleksandrkurta/Университет/3.2/Комп'ютерні системи/Лабораторки/Лабораторна№1/File3/file3.txt";
             Console.WriteLine("Перший файл");
-            Base64Convert(@"/Users/oleksandrkurta/Университет/3.2/Комп'ютерні системи/Лабораторки/Лабораторна№1/File1/file1.tar.bz2");
-            Base64(@"/Users/oleksandrkurta/Университет/3.2/Комп'ютерні системи/Лабораторки/Лабораторна№1/File1/file1.tar.bz2");
+            myCountClass.SymbolsCount(Read(path1file));
+            myCountClass.Probability(first, Read(path1file).Length);
+            DictionaryOut(first);
+            Console.WriteLine($"Ентропія - {myCountClass.Entrophy(first)},");
+            AmountOut(AmountOfInformation(Read(path1file).Length, myCountClass.Entrophy(first)), path1file);
             Console.WriteLine("Другий файл");
-            Base64Convert(@"/Users/oleksandrkurta/Университет/3.2/Комп'ютерні системи/Лабораторки/Лабораторна№1/File2/file2.tar.bz2");
-            Base64(@"/Users/oleksandrkurta/Университет/3.2/Комп'ютерні системи/Лабораторки/Лабораторна№1/File2/file2.tar.bz2");
+            myCountClass.SymbolsCount(Read(path2file));
+            myCountClass.Probability(second, Read(path2file).Length);
+            DictionaryOut(second);
+            Console.WriteLine($"Ентропія - {myCountClass.Entrophy(second)}");
+            AmountOut(AmountOfInformation(Read(path2file).Length, myCountClass.Entrophy(second)), path2file);
             Console.WriteLine("Третій файл");
-            Base64Convert(@"/Users/oleksandrkurta/Университет/3.2/Комп'ютерні системи/Лабораторки/Лабораторна№1/File3/file3.tar.bz2");
-            Base64(@"/Users/oleksandrkurta/Университет/3.2/Комп'ютерні системи/Лабораторки/Лабораторна№1/File3/file3.tar.bz2");
+            myCountClass.SymbolsCount(Read(path3file));
+            myCountClass.Probability(third, Read(path3file).Length);
+            DictionaryOut(third);
+            Console.WriteLine($"Ентропія - {myCountClass.Entrophy(third)}");
+            AmountOut(AmountOfInformation(Read(path3file).Length, myCountClass.Entrophy(third)), path3file);
+
+
+
+
+
+
+
         }
     }
 }
