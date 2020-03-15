9703dc577d5f4d30143def610f790220124cc3a9
sashakurta <34400974+sashakurta@users.noreply.github.com>
2020-03-15 16:20:01 +0200

    Lab2.3

--- Task3.cs
+++ Task3.cs
@@ -1,13 +1,101 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-namespace CSLab2
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Part3
 {
     class Program
     {
-
-        static Int64 Multiplication(int multiplicand, int multiplier)
+        static void Main(string[] args)
         {
-            Int64 product = 0;
+            Console.WriteLine("Enter A number:");
+            float multiplicandF = float.Parse(Console.ReadLine());
+            Console.WriteLine("Enter B number:");
+            float multiplierF = float.Parse(Console.ReadLine());
+            Console.WriteLine("Result is: {0}", IEEE.MultiplyIEEE(multiplicandF, multiplierF));
+            Console.ReadKey();
+        }
+    }
+    public class IEEE
+    {
+        public static float MultiplyIEEE(float multiplicand, float multiplier)
+        {
+            byte[] multiplicandBit = BitConverter.GetBytes(multiplicand);
+            byte[] multiplierBit = BitConverter.GetBytes(multiplier);
+            int mantissa1, mantissa2;
+            int sign1, sign2;
+            int exp1, exp2;
+            int res1 = BitConverter.ToInt32(multiplicandBit, 0);
+
+            int res2 = BitConverter.ToInt32(multiplierBit, 0);
+            const int bias = 127;
+
+            sign1 = res1 & Convert.ToInt32("10000000000000000000000000000000", 2);
+            sign2 = res2 & Convert.ToInt32("10000000000000000000000000000000", 2);
+            exp1 = res1 & Convert.ToInt32("01111111100000000000000000000000", 2);
+            exp1 >>= 23;
+            exp2 = res2 & Convert.ToInt32("01111111100000000000000000000000", 2);
+            exp2 >>= 23;
+
+
+            mantissa1 = res1 & Convert.ToInt32("00000000011111111111111111111111", 2) | Convert.ToInt32("00000000100000000000000000000000", 2);
+
+            mantissa2 = res2 & Convert.ToInt32("00000000011111111111111111111111", 2) | Convert.ToInt32("00000000100000000000000000000000", 2);
+
+            if (multiplicand == 0 || multiplier == 0)
+            {
+                return 0f;
+            }
+
+            Console.WriteLine("Вхідні значення:");
+            Console.WriteLine("Множене : Significand = " + Convert.ToString(sign1, 2) + " Exponent = " + Convert.ToString(exp1, 2) + " Mantissa = " + Convert.ToString(mantissa1, 2));
+            Console.WriteLine("Множник : Significand = " + Convert.ToString(sign2, 2) + " Exponent = " + Convert.ToString(exp2, 2) + " Mantissa = " + Convert.ToString(mantissa2, 2));
+
+            Console.WriteLine("COMPUTE EXPONENTS:");
+            int exponent = exp1 + exp2 - bias;
+            Console.WriteLine(exp1 + " + " + exp2 + " - 127 = " + Convert.ToString(exponent, 2) + "(2)\n");
+
+            Console.WriteLine("MULTIPLY SIGNIFICANDS:");
+            int significand = sign1 ^ sign2;
+            Console.WriteLine(Convert.ToString(sign1, 2) + " ^ " + Convert.ToString(sign2, 2) + " = " + Convert.ToString(significand, 2) + "\n");
+
+            Console.WriteLine("NORMALIZE RESULT:");
+            long mantisaLong = ShiftRightForIEEE(mantissa1, mantissa2);
+            int mantisa = 0;
+            Console.WriteLine("Mantissa = " + Convert.ToString(mantisaLong, 2));
+            if ((mantisaLong & 0x800000000000) == 0x800000000000)//чи є 47 біт "1"
+            {
+                Console.WriteLine("Exponent = " + exponent + " +1");
+                exponent++;
+            }
+            else
+                mantisaLong <<= 1;
+
+            for (int i = 0; i < 24; i++)
+            {
+                if ((mantisaLong & 0x1000000) == 0x1000000)
+                {
+                    mantisa |= 0x800000;
+                }
+                if (i == 23)
+                    break;
+                mantisa >>= 1;
+                mantisaLong >>= 1;
+            }
+            mantisa &= ~(1 << 23);
+            Console.WriteLine("Final mantissa = " + "1." + Convert.ToString(mantisa, 2) + "\n");
+            Console.WriteLine("RESULT:");
+            Console.WriteLine(Convert.ToString(significand, 2) + " " + Convert.ToString(exponent, 2) + " " + Convert.ToString(mantisa, 2));
+            int res = significand | (exponent << 23) | mantisa;
+            byte[] b = BitConverter.GetBytes(res);
+            return BitConverter.ToSingle(b, 0);
+        }
+        public static long ShiftRightForIEEE(int multiplicand, int multiplier)
+        {
+            long product = 0;
             for (int i = 32, n = 0; i > 0; i--, n++)
             {
 
@@ -23,133 +111,8 @@ namespace CSLab2
                 multiplier >>= 1;
                 String result = Convert.ToString(product, 2);
                 result = new string('0', 64 - result.Length) + result;
-
-
             }
             return product;
-
-        }
-        static void ThirdTask()
-        {
-
-            Console.WriteLine("First number int");
-            Int32 integer1 = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("First number fraction");
-            double fractional1 = Convert.ToDouble(Console.ReadLine());
-            //знак
-            int sign1 = Sign(integer1);
-            //экспонента
-            int exp1 = Exponent(integer1);
-            //мантисса
-            string integ1 = Convert.ToString(integer1, 2);
-            string fr1 = Convert.ToString(Fractonal(fractional1));
-            string Mantissa1 = "";
-            if (fr1.Length > (24 - integ1.Length))
-            {
-                Mantissa1 = integ1.Substring(1) + fr1.Substring(0, 24 - integ1.Length);
-            }
-            else
-            {
-                Mantissa1 = integ1.Substring(1) + fr1 + new string('0', (23 - Mantissa1.Length));
-            }
-
-            Console.WriteLine("1, " + Mantissa1);
-
-
-            Console.WriteLine("Second number int");
-            Int32 integer2 = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Second number fraction");
-            double fractional2 = Convert.ToDouble(Console.ReadLine());
-            //знак
-            int sign2 = Sign(integer2);
-            //экспонента
-            int exp2 = Exponent(integer2);
-            //мантисса
-            string integ2 = Convert.ToString(integer2, 2);
-            string fr2 = Convert.ToString(Fractonal(fractional2));
-            string Mantissa2 = "";
-            if (fr2.Length > (24 - integ2.Length))
-            {
-                Mantissa2 = integ2.Substring(1) + fr2.Substring(0, 24 - integ2.Length);
-            }
-            else
-            {
-                Mantissa2 = integ2.Substring(1) + fr2 + new string('0', (23 - Mantissa2.Length));
-            }
-            Console.WriteLine("1, " + Mantissa2);
-            //вычисления знака результата
-            int resSign = sign1 ^ sign2;
-            Console.WriteLine("Multiply significands {0} = {1}^{2}", resSign, sign1, sign2);
-            //вычисления мантиссы результата
-            int resMantisa1 = ConvertToBase10('1' + Mantissa1);
-            int resMantissa2 = ConvertToBase10('1' + Mantissa2);
-            Int64 resMantisa = Multiplication(resMantisa1, resMantissa2);
-
-            Console.WriteLine("Mantissa" + Convert.ToString(resMantisa, 2).Substring(1, 23));
-            //вычисление экспоненты
-            int resexp = exp1 + exp2 - 127;
-            Console.WriteLine("Exponenta" + resexp);
-
-            static int ConvertToBase10(string Mantissa)
-            {
-                int number = 0;
-                for (int i = 1, n = Mantissa.Length; n != 0; n--, i *= 2)
-                {
-                    if (Mantissa[n - 1] == '1')
-                    {
-                        number += i;
-                    }
-                }
-                return number;
-            }
-            static int Sign(int A)
-            {
-                int sign;
-                if (A > 0)
-                {
-                    sign = 0;
-                }
-                else
-                {
-                    sign = 1;
-                }
-                return sign;
-            }
-            static int Exponent(int A)
-            {
-                string str = Convert.ToString(A, 2);
-                return 127 + (str.Length - 1);
-            }
-            static string Fractonal(double B)
-            {
-                string str = "";
-                while (B != 1)
-                {
-                    B *= 2;
-                    if (B > 1)
-                    {
-                        B -= 1;
-                        str += "1";
-                    }
-                    else if (B < 1)
-                    {
-                        str += "0";
-                    }
-
-                }
-                str += "1";
-
-                return str;
-            }
-        }
-
-        static void Main()
-        {
-            ThirdTask();
-
-
         }
     }
 }
